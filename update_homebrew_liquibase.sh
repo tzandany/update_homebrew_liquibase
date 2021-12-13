4acb198f8c588541d78a77e87591cae20216c8fd
tzandany <57370754+tzandany@users.noreply.github.com>
2021-12-13 11:54:11 -0600

    Update update_homebrew_liquibase.sh

--- update_homebrew_liquibase.sh
+++ update_homebrew_liquibase.sh
@@ -36,7 +36,7 @@ end" > liquibase.rb
 git clone https://github.com/szandany/update_homebrew_liquibase_formulae
 cd update_homebrew_liquibase_formulae
 git remote add tzandany https://github.com/tzandany/update_homebrew_liquibase_formulae.git
-git config --global user.email szandany@liquibase.com; git config --global user.name szandany; git config pull.rebase false
+git config --global user.email thezandan@gmail.com; git config --global user.name tzandany; git config pull.rebase false
 git checkout -b update_lb_${VERSION}
 # cp -rf liquibase.rb homebrew-core/liquibase.rb
 cp -rf ../liquibase.rb homebrew-core/liquibase.rb
